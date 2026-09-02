1f03ea49913365d66284ab638e93bd7d34071f51
grillgrut <grillgrut@gmail.com>
2026-09-02 11:26:11 -0500

    Setup the inital file structure as well as webpack

---
+++ src/createTodo.js
@@ -0,0 +1,9 @@
+function createTodo (title) {
+    const name = title;
+
+    return { name };
+}
+
+export { createTodo };
+
+
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { createTodo } from "./createTodo.js";
+
+const firstTask = createTodo("Hello");
+console.log(firstTask);
